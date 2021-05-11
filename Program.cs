9a3e8c29270700c0a526b8fcf353b355efbd9eae
Артем Городков <gorodkoff44@gmail.com>
2021-05-11 14:21:20 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ekz_Gorodkov_MinNechetn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+				Console.WriteLine("Задание: Минимальный из элементов массива с нечетными индексами");
+				Console.WriteLine();
+				Console.WriteLine("Введите длину массива");
+				int m = int.Parse(Console.ReadLine());
+				if (m < 1) //Проверка
+				{
+					Console.WriteLine("Длина массива не может быть меньше 1");
+					return;
+				}
+				int[] mass = new int[m]; //Массив
+				Random rnd = new Random(); //Случайные значения
+				int r = rnd.Next(-100, 100); //Первое случайное значение
+				int min; //Будет хранить минимальнле значение
+				Console.WriteLine();
+				Console.WriteLine("Массив:");
+				//Console.WriteLine();
+				for (int i = 0; i < m; i++) //Перебор индексов массива
+				{
+					mass[i] = r; //заполнение массива
+					r = rnd.Next(-100, 100); //Следующее случайное значение
+					Console.Write($"{mass[i]} [{i}]  ");
+				}
+
+				//Проверка на наличие первого нечетного индекса
+				if (m > 1)
+					min = mass[1];
+				else
+				{
+					Console.WriteLine("Здесь нет чисел с нечетными индексами!");
+					return;
+				}
+				for (int i = 1; i < m; ++i) //перебор нечетных индексов массива массива
+				{
+					if (mass[i] < min) //если значение меньше первого нечетного, то оно минимальное
+					{
+						min = mass[i];
+					}
+					++i; //пропуск четного значения
+				}
+				Console.WriteLine();
+				Console.WriteLine($"Минимальное число с нечетным индексом: {min}");
+				Console.ReadKey();
+			}
+    }
+}
